02bae2c81763df3e0e56264066f34285c943c3cc
joepasss <joepasss@gmail.com>
2024-10-26 08:18:45 +0900

    get screensize & detatch header

--- src/main.c
+++ src/main.c
@@ -1,23 +1,18 @@
+#include "jovim.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
-/*** defines ***/
-#define CTRL_KEY(k) ((k) & 0x1f)
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
 
-struct termios orig_termios;
-
-/*** prototypes ***/
-void die(const char *s);
-void enableRawMode();
-void disableRawMode();
-void editorRefreshScreen();
-char editorReadKey();
-void editorProcessKeyPress();
-
-/*** implements ***/
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -30,19 +25,19 @@ void die(const char *s) {
 }
 
 void disableRawMode() {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios)) {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios)) {
     die("tcsetattr");
   }
 }
 
 void enableRawMode() {
-  if (tcgetattr(STDIN_FILENO, &orig_termios)) {
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios)) {
     die("tcgetattr");
   };
 
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
 
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
@@ -56,11 +51,36 @@ void enableRawMode() {
   }
 }
 
+int getWindowSize(int *rows, int *cols) {
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    return -1;
+  } else {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+
+    return 0;
+  }
+}
+
 /*** output ***/
 
 void editorRefreshScreen() {
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorDrawRows() {
+  int y;
+
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
 }
 
 /*** input ***/
@@ -93,6 +113,11 @@ void editorProcessKeyPress() {
 
 int main() {
   enableRawMode();
+
+  // init
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1) {
+    die("getWindowSize");
+  }
 
   while (1) {
     editorRefreshScreen();
