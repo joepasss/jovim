d2fcf431e439b14daceb7e9b057c6038e26c9c5c
joepasss <joepasss@gmail.com>
2024-11-15 12:40:02 +0900

    add home key & end key function

--- src/input.c
+++ src/input.c
@@ -30,10 +30,18 @@ int editorReadKey() {
 
         if (seq[2] == '~') {
           switch (seq[1]) {
+          case '1':
+            return HOME_KEY;
+          case '4':
+            return END_KEY;
           case '5':
             return PAGE_UP;
           case '6':
             return PAGE_DOWN;
+          case '7':
+            return HOME_KEY;
+          case '8':
+            return END_KEY;
           }
         }
       } else {
@@ -46,7 +54,18 @@ int editorReadKey() {
           return ARROW_RIGHT;
         case 'D':
           return ARROW_LEFT;
+        case 'H':
+          return HOME_KEY;
+        case 'F':
+          return END_KEY;
         }
+      }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H':
+        return HOME_KEY;
+      case 'F':
+        return END_KEY;
       }
     }
 
@@ -92,6 +111,14 @@ void editorProcessKeyPress(struct editorConfig *E) {
     write(STDOUT_FILENO, "\x1b[H", 3);
 
     exit(0);
+    break;
+
+  case HOME_KEY:
+    E->cx = 0;
+    break;
+
+  case END_KEY:
+    E->cx = E->screencols - 1;
     break;
 
   case PAGE_UP:
